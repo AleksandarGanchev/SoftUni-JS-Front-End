24ae08e97abeb0980cb8e6a7c8a62900b2f6b217
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-06-01 14:13:37 +0100

    Create 05. Math Operations.js

---
+++ JS Front-End - Exercise/JS Syntax Fundamentals - Lab/05. Math Operations.js
@@ -0,0 +1,25 @@
+function mathOperations(firstNumber, secondNumber, operator) {
+    if (operator == "+") {
+        result = firstNumber + secondNumber;
+
+    } else if (operator == "-") {
+        result = firstNumber - secondNumber;
+
+    } else if (operator == "-") {
+        result = firstNumber - secondNumber;
+
+    } else if (operator == "*") {
+        result = firstNumber * secondNumber;
+
+    } else if (operator == "/") {
+        result = firstNumber / secondNumber;
+
+    } else if (operator == "%") {
+        result = firstNumber % secondNumber;
+
+    } else if (operator == "**") {
+        result = firstNumber ** secondNumber;
+    }
+    
+    console.log(result)
+}
