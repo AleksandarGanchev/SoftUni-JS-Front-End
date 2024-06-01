d9773de9de90a37f8faeec070e2d2632f95a6483
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-06-01 20:36:13 +0100

    Update 04. Print And Sum.js

--- JS Front-End - Exercise/JS Syntax Fundamentals - Exercises/04. Print And Sum.js
+++ JS Front-End - Exercise/JS Syntax Fundamentals - Exercises/04. Print And Sum.js
@@ -1,11 +1,11 @@
 function solve(num1, num2) {
     let i = num1;
-    let numbers = [i]
+    let numbers = [i];
     let totalSum = num1;
 
     while (i < num2) {
         i++;
-        numbers.push(i)
+        numbers.push(i);
         totalSum += i;   
     }
 
