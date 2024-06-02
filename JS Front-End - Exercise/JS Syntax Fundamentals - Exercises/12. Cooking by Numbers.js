a7e3ac36221e6f4b0e209016bb38f7cbede8ae8f
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-06-02 11:53:45 +0100

    improved solution

--- JS Front-End - Exercise/JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
+++ JS Front-End - Exercise/JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
@@ -1,4 +1,4 @@
-function solve(number, op1, op2, op3, op4, op5) {
+function solve(number, ...ops) {
     let result = Number(number);
 
     const operations = {
@@ -9,10 +9,28 @@ function solve(number, op1, op2, op3, op4, op5) {
         "fillet": (num) => num * 0.8
     };
 
-    const ops = [op1, op2, op3, op4, op5];
-
     for (let i = 0; i < ops.length; i++) {
         result = operations[ops[i]](result);
         console.log(result);
     }
 }
+
+
+// function solve(number, op1, op2, op3, op4, op5) {
+//     let result = Number(number);
+
+//     const operations = {
+//         "chop": (num) => num / 2,
+//         "dice": (num) => Math.sqrt(num),
+//         "spice": (num) => num + 1,
+//         "bake": (num) => num * 3,
+//         "fillet": (num) => num * 0.8
+//     };
+
+//     const ops = [op1, op2, op3, op4, op5];
+
+//     for (let i = 0; i < ops.length; i++) {
+//         result = operations[ops[i]](result);
+//         console.log(result);
+//     }
+// }
