4db27d77c54fb03939e5fbf630bfed0ff5e8725f
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-06-02 11:49:17 +0100

    Create 12. Cooking by Numbers.js

---
+++ JS Front-End - Exercise/JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
@@ -0,0 +1,18 @@
+function solve(number, op1, op2, op3, op4, op5) {
+    let result = Number(number);
+
+    const operations = {
+        "chop": (num) => num / 2,
+        "dice": (num) => Math.sqrt(num),
+        "spice": (num) => num + 1,
+        "bake": (num) => num * 3,
+        "fillet": (num) => num * 0.8
+    };
+
+    const ops = [op1, op2, op3, op4, op5];
+
+    for (let i = 0; i < ops.length; i++) {
+        result = operations[ops[i]](result);
+        console.log(result);
+    }
+}
