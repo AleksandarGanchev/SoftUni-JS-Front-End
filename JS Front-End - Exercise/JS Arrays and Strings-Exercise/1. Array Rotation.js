f197c453558a8c661902e3efb42caeeed9c26157
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-06-03 23:36:29 +0100

    Create 1. Array Rotation.js

---
+++ JS Front-End - Exercise/JS Arrays and Strings-Exercise/1. Array Rotation.js
@@ -0,0 +1,9 @@
+function solve(array, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        let element  = array.shift();
+        array.push(element)    
+    }
+    
+    console.log(array.join(" "));
+}
+
