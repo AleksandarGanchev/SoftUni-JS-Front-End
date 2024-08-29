f1abeccf4b01f4231cfd37f313dcc9b41121ab99
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-08-29 11:10:53 +0100

    Create 04. Extend Prototype.js

---
+++ JS Front-End - Exercise/Prototypes and Inheritance - Lab/04. Extend Prototype.js
@@ -0,0 +1,6 @@
+function solve(classObj) {
+    classObj.prototype.species = 'Human';
+    classObj.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ` + this.toString();
+        };
+}   
